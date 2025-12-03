8eb80f37006578e5229237324eda207bf5ea71a6
vick <vinksans2@gmail.com>
2025-12-03 20:21:27 -0300

    Add build script

--- sistema_alumnos/build.sh
+++ sistema_alumnos/build.sh
@@ -7,4 +7,7 @@ pip install -r requirements.txt
 echo "=== APLICANDO MIGRACIONES ==="
 python manage.py migrate
 
+echo "=== COLECTANDO ARCHIVOS ESTÁTICOS ==="
+python manage.py collectstatic --noinput
+
 echo "=== BUILD COMPLETADO ==="
